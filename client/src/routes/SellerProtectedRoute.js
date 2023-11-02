a24229907d90e810dbcf41e04f10c96bbce9a111
ismail <ielfakir49@gmail.com>
2023-11-02 20:06:38 +0100

    adding Loader animation

--- client/src/routes/SellerProtectedRoute.js
+++ client/src/routes/SellerProtectedRoute.js
@@ -1,10 +1,12 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-// import Loader from "../components/Layout/Loader";
+import Loader from "../components/Layout/Loader";
 
 const SellerProtectedRoute = ({ children }) => {
   const { isLoading, isSeller } = useSelector((state) => state.seller);
-  if (isLoading === false) {
+  if (isLoading === true) {
+    return <Loader />;
+  } else {
     if (!isSeller) {
       return <Navigate to={`/seller-login`} replace />;
     }
