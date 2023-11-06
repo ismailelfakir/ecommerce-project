5b820c6045fe21b231528f635c25dc0bc17dfcd3
ismail <ielfakir49@gmail.com>
2023-11-06 18:04:39 +0100

    CRUD Event /{Update not complet}

---
+++ client/src/pages/Seller/SellerAllEvents.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import DashboardHeader from '../../components/Seller/Layout/DashboardHeader'
+import DashboardSideBar from '../../components/Seller/Layout/DashboardSideBar'
+import AllEvents from "../../components/Seller/AllEvents";
+
+const SellerAllEvents = () => {
+    return (
+        <div>
+            <DashboardHeader />
+            <div className="flex justify-between w-full">
+                <div className="w-[80px] 800px:w-[330px]">
+                  <DashboardSideBar active={5} />
+                </div>
+                <div className="w-full justify-center flex">
+                    <AllEvents />
+                </div>
+              </div>
+        </div>
+      )
+}
+
+export default SellerAllEvents
